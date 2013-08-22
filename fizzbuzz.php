676f2556f81ab4b9db1eaae16f96d1714e0cac07
kobayashi <koba.s0125@gmail.com>
2013-08-22 15:36:28 +0900

    make echoFizzBuzz function

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -7,22 +7,23 @@ $number = $argv[1];
 if (! preg_match('/^\d*$/', $number)) exit('input error');
 
 for($i = 1; $i < $number + 1; $i++) {
-
-    if ($i % 15 == 0) {
-        echo 'FizzBuzz',PHP_EOL;
-        continue;
-    }
-
-    if ($i % 3 == 0) {
-        echo 'Fizz',PHP_EOL;
-        continue;
-    }
-
-    if ($i % 5 == 0) {
-        echo 'Bizz',PHP_EOL;
-        continue;
-    }
-
-    echo $i, PHP_EOL;
+    echoFizzBuzz($i);
 }
 
+function echoFizzBuzz($num) {
+    if ($num % 15 == 0) {
+        echo 'FizzBuzz',PHP_EOL;
+        return;
+    }
+
+    if ($num % 3 == 0) {
+        echo 'Fizz',PHP_EOL;
+        return;
+    }
+
+    if ($num % 5 == 0) {
+        echo 'Bizz',PHP_EOL;
+        return;
+    }
+    echo $num, PHP_EOL;
+}
